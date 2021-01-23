82d8a87ea7bf66113e04460558a3d983d3157f59
lockednlevered <shaynel@sfu.ca>
2021-01-23 12:56:48 -0800

    read rom file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let pif_file_name = env::args().nth(1).unwrap();
+    let rom_file_name = env::args().nth(2).unwrap();
+    
+    let pif = read_binary(pif_file_name);
+    let rom =read_binary(rom_file_name);
+    
+}
+
+fn read_binary<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = File::open(path.as_ref()).unwrap();
+    let mut file_buffer = Vec::new();
+    file.read_to_end(&mut file_buffer).unwrap();
+    file_buffer 
 }
